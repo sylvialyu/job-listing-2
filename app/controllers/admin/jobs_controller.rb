984e0f7a97ca65ffd409055e59d4039aeda89906
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-03-13 12:52:49 +0800

    implement part4

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -3,7 +3,7 @@ class Admin::JobsController < ApplicationController
   before_filter :require_is_admin
 
   def index
-    @jobs = Job.all
+    @jobs = Job.all.order("created_at DESC")
   end
 
   def new
@@ -46,7 +46,7 @@ class Admin::JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
 
 end
